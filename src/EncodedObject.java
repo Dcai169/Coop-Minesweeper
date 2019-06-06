a381309eba961194bc406092326a81320a8e3f2d
Dcai <legoguy145@gmail.com>
2019-06-06 13:29:35 -0400

    refactored Parser to EncodedObject and added more helper methods

--- src/EncodedObject.java
+++ src/EncodedObject.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
 
-public class Parser {
+public class EncodedObject {
+
+    private String origin, className, header, body;
+    private Object object;
+
+    public EncodedObject(String originName, Object object) {
+        this.origin = originName;
+        this.className = object.getClass().getSimpleName();
+        this.object = object;
+        this.body = null;
+        this.header = null;
+    }
+
+    @Override
+    public String toString() {
+        return getPacket(origin, object);
+    }
+
     public static Action constructAction(String toString){
         int r = -1;
         int c = -1;
@@ -100,5 +117,53 @@ public class Parser {
             }
         }
         return new Board(width, height, mines);
+    }
+
+    public static String getHeaderString(String origin, Object obj){
+        return "<h(origin:"+origin+";class:"+obj.getClass().getSimpleName()+";)/h>";
+    }
+
+    public static String getBodyString(Object obj){
+        return "<b("+obj.toString()+")/b>";
+    }
+
+    public static String getPacket(String origin, Object object){
+        return getHeaderString(origin, object)+getBodyString(object);
+    }
+
+
+
+
+    public String getHeader(String packet){
+        header = packet.substring(packet.indexOf("<h(")+"<h(".length(), packet.indexOf(")/h>"));
+        return packet.substring(packet.indexOf("<h(")+"<h(".length(), packet.indexOf(")/h>"));
+    }
+
+    public String getBody(String packet){
+        body = packet.substring(packet.indexOf("<b(")+"<b(".length(), packet.indexOf(")/b>"));
+        return packet.substring(packet.indexOf("<b(")+"<b(".length(), packet.indexOf(")/b>"));
+    }
+
+    public void fillFields(String header, String body){
+        while (header.contains(":") && header.contains(";")){
+            String key = header.substring(0, header.indexOf(":"));
+            String value = header.substring(header.indexOf(":")+1, header.indexOf(";"));
+            header = header.substring(header.indexOf(";")+1);
+            if ("origin".equals(key)) {
+                origin = value;
+                break;
+            } else if ("class".equals(key)) {
+                className = value;
+                break;
+            } else {
+                break;
+            }
+        }
+
+        if (className.equals("Board")){
+            object = constructBoard(body);
+        } else if (className.equals("Action")){
+            object = constructAction(body);
+        }
     }
 }
