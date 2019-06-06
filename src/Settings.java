180215734151c813fdf99c03a18faac7b706867d
MrTrainCow <legoguy145@gmail.com>
2019-06-05 23:14:52 -0400

    added listeners to Settings

--- src/Settings.java
+++ src/Settings.java
@@ -5,4 +5,6 @@ public class Settings {
     public static final int SIZE = 50;
     public static final String ADDRESS = "127.0.0.1";
     public static final int PORT = 5005;
+    public static UDPListener LISTENER = new UDPListener(ADDRESS, PORT);
+    public static UDPListener VOID_LISTENER = new UDPListener("0.0.0.0", 0);
 }
