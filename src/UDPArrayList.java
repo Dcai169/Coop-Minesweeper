4cfd083b12bf2276c2fa83b53ad3b630896affa9
MrTrainCow <legoguy145@gmail.com>
2019-06-05 23:16:37 -0400

    added way to add elements w/o sending packets
    another way to do this is to cast the UDPArrayList to an ArrayList and call add() on that

--- src/UDPArrayList.java
+++ src/UDPArrayList.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 
-public class UDPArrayList<E> extends ArrayList<E> implements ThreadCompleteListener{
+public class UDPArrayList<E> extends ArrayList<E> {
 
     private UDPListener listener;
 
     public UDPArrayList(UDPListener listener){
         super();
         this.listener = listener;
-        listener.addListener(this);
     }
 
     @Override
@@ -18,7 +17,17 @@ public class UDPArrayList<E> extends ArrayList<E> implements ThreadCompleteListe
     }
 
     @Override
-    public void notifyOfThreadComplete(Thread thread) {
-        super.add((E) listener.getData());
+    public void add(int i, E data){
+        if (i >= 0){
+            UDPTransmission tx = new UDPTransmission(Settings.ADDRESS, Settings.PORT, data.toString());
+            tx.doRun();
+            super.add(i, data);
+        } else {
+            super.add(-i,data);
+        }
+    }
+
+    public UDPListener getListener() {
+        return listener;
     }
 }
