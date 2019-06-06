89647d0bde1c453d6a2857411c21888e3db75108
Dcai <legoguy145@gmail.com>
2019-06-06 10:20:13 -0400

    Revert "added continueListening boolean"
    This reverts commit e19c519673c640c006fdb0dd9ad6f00799d5c3f7.

--- src/Settings.java
+++ src/Settings.java
@@ -3,9 +3,8 @@ public class Settings {
     public static final int WIDTH = 10;
     public static final int HEIGHT = 8;
     public static final int SIZE = 50;
-
     public static final String ADDRESS = "127.0.0.1";
     public static final int PORT = 5005;
-    public static final UDPListener LISTENER = new UDPListener(ADDRESS, PORT, true);
-    public static final UDPListener VOID_LISTENER = new UDPListener("0.0.0.0", 0, true);
+    public static UDPListener LISTENER = new UDPListener(ADDRESS, PORT);
+    public static UDPListener VOID_LISTENER = new UDPListener("0.0.0.0", 0);
 }
