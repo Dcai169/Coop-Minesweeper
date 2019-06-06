6637c43feb37adc016d48ff4806a640e08e17c7e
MrTrainCow <legoguy145@gmail.com>
2019-06-06 19:10:02 -0400

    testing

--- src/TestMain.java
+++ src/TestMain.java
@@ -53,8 +53,8 @@ public class TestMain {
 //        System.out.println(EncodedObject.getHeaderString("Foxtrot", new Mine(5, 3)));
 //        System.out.println(EncodedObject.getHeaderString("Foxtrot", ""));
 //        String data = EncodedObject.getPacket("Foxtrot", new Mine(5, 3));
-        String string = "width:500;height:400;mines:[r$7#c$6#, r$1#c$5#, r$1#c$3#, r$3#c$0#, r$6#c$6#, r$7#c$5#, r$4#c$2#, r$4#c$2#, r$2#c$1#, r$5#c$0#];";
+        String string = "width:500;height:400;mines:[r{1}c{9}, r{2}c{2}, r{1}c{7}, r{2}c{2}, r{7}c{9}, r{1}c{9}, r{0}c{2}, r{5}c{7}, r{0}c{5}, r{4}c{2}];";
         Board b = EncodedObject.constructBoard(string);
-        System.out.println(b.getMines());
+        System.out.println(b.toString());
     }
 }
