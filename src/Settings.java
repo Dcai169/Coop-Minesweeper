f324595449da2f20b9b70a0fc08a96d55061e3ad
MrTrainCow <legoguy145@gmail.com>
2019-06-06 20:58:59 -0400

    added more fields in ClientSettings

--- src/Settings.java
+++ src/Settings.java
@@ -4,10 +4,10 @@ public class Settings {
     public static final int HEIGHT = 8;
     public static final int SIZE = 50;
     public static final String LOCALHOST = "127.0.0.1";
-    public static final String TARGET_ADDRESS = "192.168.29.237";
-    public static final String INCOMING_ADDRESS = "192.168.29.101";
+    public static final String TARGET_ADDRESS = ClientSettings.TARGET_ADDRESS;
+    public static final String INCOMING_ADDRESS = ClientSettings.INCOMING_ADDRESS;
     public static final int PORT = 5005;
     public static UDPListener LISTENER = new UDPListener(INCOMING_ADDRESS, PORT);
     public static UDPListener VOID_LISTENER = new UDPListener("0.0.0.0", 0);
-    public static String NAME = Name.NAME;
+    public static String NAME = ClientSettings.NAME;
 }
