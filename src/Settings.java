af19c30cdc444df2cf156c60dcf6293cba521314
Dcai <legoguy145@gmail.com>
2019-06-06 21:33:36 -0400

    removed vestigial lines

--- src/Settings.java
+++ src/Settings.java
@@ -1,13 +1,10 @@
-public class Settings {
+public class Settings extends ClientSettings{
     public static final int TOTAL_MINES = 10;
     public static final int WIDTH = 10;
     public static final int HEIGHT = 8;
     public static final int SIZE = 50;
     public static final String LOCALHOST = "127.0.0.1";
-    public static final String TARGET_ADDRESS = ClientSettings.TARGET_ADDRESS;
-    public static final String INCOMING_ADDRESS = ClientSettings.INCOMING_ADDRESS;
     public static final int PORT = 5005;
     public static UDPListener LISTENER = new UDPListener(INCOMING_ADDRESS, PORT);
     public static UDPListener VOID_LISTENER = new UDPListener("0.0.0.0", 0);
-    public static String NAME = ClientSettings.NAME;
 }
