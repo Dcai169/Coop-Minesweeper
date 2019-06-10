191be758cf99d819bf95378bcc2e6d8b496e0a6e
Dcai <legoguy145@gmail.com>
2019-06-10 15:32:43 -0400

    added src/ClientSettings.java

---
+++ src/ClientSettings.java
@@ -0,0 +1,11 @@
+public class ClientSettings {
+    public static final String TARGET_ADDRESS = "172.16.123.2";
+    public static final String INCOMING_ADDRESS = "172.16.123.1";
+    public static final boolean IS_SERVER = false;
+    public static final String NAME = "Client";
+
+//    public static final String TARGET_ADDRESS = "172.16.123.1";
+//    public static final String INCOMING_ADDRESS = "172.16.123.2";
+//    public static final boolean IS_SERVER = true;
+//    public static final String NAME = "Server";
+}
